06ceb99049a75476262800499e37de329b2efed1
TRUPTI-123-CHOUDHARI <56343038+TRUPTI-123-CHOUDHARI@users.noreply.github.com>
2020-07-31 18:08:41 -0700

    Update h3home.js

--- h3home.js
+++ h3home.js
@@ -12,5 +12,13 @@ $(document).ready(function(){
         }
     })
   })
-
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var click = $(event.target);
+        var _open = $(".navbar-collapse").hasClass("show");
+        if (_open === true && !click.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
   
